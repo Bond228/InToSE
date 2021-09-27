263b78225093ba3a1ed0afc5b6e76ebd20560476
Bond228 <64462609+Bond228@users.noreply.github.com>
2021-09-28 01:04:39 +0700

    Улучшил меню, сделал переход в режим игры и добавленна музыка

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-console.log("Hello, galaga")
+var audio = new Audio();
+audio.autoplay = false;
+
+function soundClick() {
+	if(audio.autoplay != true){
+  		audio.src = 'startMusic.mp3'; 
+  		audio.autoplay = true; 
+	}
+	else{
+		audio.autoplay = false;
+		audio.pause();
+	}
+}
+
